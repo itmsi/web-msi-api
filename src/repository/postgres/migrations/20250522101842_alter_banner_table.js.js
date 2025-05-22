74b80f5bc99607518b3d53c7e5bd62b22f6c520b
abdul falaq <falaq@logique.co.id>
2025-05-22 17:27:29 +0700

    Fix: add type banner

---
+++ src/repository/postgres/migrations/20250522101842_alter_banner_table.js.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('mst_banner', (table) => {
+    table.string('banner_type', 220).nullable()
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('mst_banner', (table) => {
+    table.dropColumn('banner_type')
+  })
+};
