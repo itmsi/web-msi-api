ecda2e0f63769febcbf504b643d7c5b550ee5bf2
abdul falaq <falaq@logique.co.id>
2025-05-28 11:58:45 +0700

    Fix: add seeder add menu

---
+++ src/repository/postgres/seeders/2001-mst_admin_menu.js
@@ -0,0 +1,64 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+// eslint-disable-next-line func-names
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  // await knex('mst_admin_menu').del()
+  const admin_menu = [
+    {
+      menu_id: '37',
+      parent: '5',
+      menu_name: 'Specification',
+      menu_url: 'admin/management-member/specification',
+      menu_status: '1',
+      menu_sort: '7',
+      menu_icon: 'fas fa-cogs',
+      created_at: '2025-05-28 10:00:00.000 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null,
+      permission_name: 'specification',
+      heading_admin_menu_id: '3'
+    },
+    {
+      menu_id: '38',
+      parent: '5',
+      menu_name: 'Specification Label',
+      menu_url: 'admin/management-member/specification-label',
+      menu_status: '1',
+      menu_sort: '8',
+      menu_icon: 'fas fa-cogs',
+      created_at: '2025-05-28 10:00:00.000 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null,
+      permission_name: 'specificationlabel',
+      heading_admin_menu_id: '3'
+    },
+    {
+      menu_id: '39',
+      parent: '5',
+      menu_name: 'Specification Value',
+      menu_url: 'admin/management-member/specification-value',
+      menu_status: '1',
+      menu_sort: '9',
+      menu_icon: 'fas fa-cogs',
+      created_at: '2025-05-28 10:00:00.000 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null,
+      permission_name: 'specificationvalue',
+      heading_admin_menu_id: '3'
+    }
+  ]
+  return knex('mst_admin_menu').del()
+    .then(() => knex('mst_admin_menu').insert(admin_menu));
+};
---
+++ src/repository/postgres/seeders/9003_mst_menu_has.js
@@ -0,0 +1,134 @@
+/* eslint-disable implicit-arrow-linebreak */
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  const data = [
+    {
+      menu_id: 37,
+      permission_id: 1,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 37,
+      permission_id: 2,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 37,
+      permission_id: 3,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 37,
+      permission_id: 4,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 38,
+      permission_id: 1,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 38,
+      permission_id: 2,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 38,
+      permission_id: 3,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 38,
+      permission_id: 4,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 39,
+      permission_id: 1,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 39,
+      permission_id: 2,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 39,
+      permission_id: 3,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    },
+    {
+      menu_id: 39,
+      permission_id: 4,
+      created_at: '2025-03-11 11:54:24.776 +0700',
+      created_by: null,
+      updated_at: null,
+      updated_by: null,
+      deleted_at: null,
+      deleted_by: null
+    }
+  ]
+  return knex('mst_menu_has_permissions').del()
+    .then(() =>
+      // Inserts seed entries
+      knex('mst_menu_has_permissions').insert(data));
+};
