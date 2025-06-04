4dc9ab2af6776d677007e7256e7552f8a9dd1f24
abdul falaq <falaq@logique.co.id>
2025-06-04 09:16:47 +0700

    Fix: update endpoint for respons

--- src/static/schema/index.js
+++ src/static/schema/index.js
@@ -42,6 +42,7 @@ const specificationProduct = require('./specification_product.json')
 const specificationProductLabel = require('./specification_product_label.json')
 const specificationProductValue = require('./specification_product_value.json')
 const publicSpecification = require('./public_specification.json')
+const publicJobCareer = require('./public_job_career.json')
 
 module.exports = {
   ...auth,
@@ -87,5 +88,6 @@ module.exports = {
   ...specificationProduct,
   ...specificationProductLabel,
   ...specificationProductValue,
-  ...publicSpecification
+  ...publicSpecification,
+  ...publicJobCareer
 }
