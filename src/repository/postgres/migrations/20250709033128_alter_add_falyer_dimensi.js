b6e691004df0c40e432b37588dc35c7d5497674b
abdul falaq <falaq@logique.co.id>
2025-07-10 09:15:51 +0700

    Fix: add flayer

--- src/repository/postgres/migrations/20250709033128_alter_add_falyer_dimensi.js
+++ src/repository/postgres/migrations/20250709033128_alter_add_falyer_dimensi.js
@@ -3,9 +3,18 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.alterTable('mst_product_dimensi', (table) => {
-    table.string('product_flayer', 200).nullable();
-  })
+  return knex.raw(`
+    DO $$ 
+    BEGIN 
+      IF NOT EXISTS (
+        SELECT 1 FROM information_schema.columns 
+        WHERE table_name = 'mst_product_dimensi' 
+        AND column_name = 'product_flayer'
+      ) THEN
+        ALTER TABLE mst_product_dimensi ADD COLUMN product_flayer varchar(200) null;
+      END IF;
+    END $$;
+  `);
 };
 
 /**
